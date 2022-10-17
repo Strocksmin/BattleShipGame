8df8d48ab21e632e3a0308a4fee38bc4f7ee9d5c
Andrey <karabanovae2120@gmail.com>
2022-10-17 20:02:34 +0300

    Added testConvertColumnInput, testConvertRowInput, testGame

--- src/main/java/com/company/main/Main.java
+++ src/main/java/com/company/main/Main.java
@@ -1,8 +1,157 @@
 package com.company.main;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+        List<Player> gamePlayers = new ArrayList<>();
+        gamePlayers.add(player1);
+        gamePlayers.add(player2);
+
+        Scanner input = new Scanner(System.in);
+        String firstCoordinate;
+        String secondCoordinate;
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = 0;
+        int endCol = 0;
+
+        for (Player player : gamePlayers) {
+            System.out.println(player.name + ", расположите свои корабли");
+            System.out.println();
+            player.gameBoard.printGameBoard();
+
+            for (Battleships ship : player.ships) {
+                System.out.printf("Укажите координаты %s (%d cells):%n", ship.name, ship.length);
+                System.out.println();
+                boolean inputIsGood = false;
+
+                while (!inputIsGood) {
+                    firstCoordinate = input.next().toUpperCase();
+                    secondCoordinate = input.next().toUpperCase();
+
+                    if (firstCoordinate.length() > 3 || secondCoordinate.length() > 3 || convertRowInput(firstCoordinate) > 9 || convertColumnInput(firstCoordinate) > 9) {
+                        System.out.printf("Ошибка! Координаты указаны неверно");
+                        continue;
+                    }
+
+                    startRow = convertRowInput(firstCoordinate);
+                    startCol = convertColumnInput(firstCoordinate);
+
+                    endRow = convertRowInput(secondCoordinate);
+                    endCol = convertColumnInput(secondCoordinate);
+
+                    int choiceShipLength = Math.abs(startCol - endCol) + Math.abs(startRow - endRow) + 1;
+
+                    if (choiceShipLength != ship.length) {
+                        System.out.printf("Ошибка! Неправильная длина %s! Попробуйте снова:%n", ship.name);
+                        continue;
+                    }
+                    if (player.gameBoard.checkProximity(startRow, startCol, endRow, endCol)) {
+                        System.out.println("Ошибка! Вы расположили корабль слишком близко к другому кораблю. Попробуйте снова:");
+                        continue;
+                    }
+
+                    inputIsGood = true;
+                }
+
+                player.gameBoard.updateBoard(startRow, startCol, endRow, endCol, ship);
+                player.gameBoard.printGameBoard();
+            }
+            clearScreenAfterEnter(input);
+
+        }
+        System.out.println("Игра начинается!");
+        System.out.println();
+
+        Player[] playerArr = new Player[2];
+        playerArr[0] = player1;
+        playerArr[1] = player2;
+        int currentPlayer = 0;
+        int oppositePlayer = 1;
+
+        boolean winGame = false;
+        while(!winGame) {
+            for (int i = 0; i < playerArr.length; i++) {
+                playerArr[oppositePlayer].gameBoard.printFogOfWarBoard();
+                System.out.println("---------------------");
+                playerArr[currentPlayer].gameBoard.printGameBoard();
+                System.out.println();
+                System.out.println(playerArr[currentPlayer].name + ", ваш ход:");
+
+                firstCoordinate = input.next().toUpperCase();
+                startRow = convertRowInput(firstCoordinate);
+                startCol = convertColumnInput(firstCoordinate);
+
+                if (firstCoordinate.length() > 2 && startCol != 9) {
+                    System.out.println("Ошибка! Вы ввели неправильные координаты! Попробуйте снова:");
+                    continue;
+                }
+                if (startRow < 0 || startRow > 9 || startCol < 0 || startCol > 9) {
+                    System.out.println("Ошибка! Вы ввели неправильные координаты! Попробуйте снова:");
+                    continue;
+                }
+                boolean hitOne = (playerArr[oppositePlayer].gameBoard.takeAShot(startRow, startCol));
+                playerArr[oppositePlayer].gameBoard.printFogOfWarBoard();
+
+                if (hitOne) {
+                    for (Battleships ship : playerArr[oppositePlayer].ships) {
+                        if (ship.isShipHit(startRow, startCol)) {
+                            ship.removeShipCoordinate(startRow, startCol);
+                            if (ship.isShipSunk()) {
+                                System.out.printf("Вы потопили %s! Выберите новую цель:%n", ship.name);
+                            } else {
+                                System.out.printf("Вы попали по кораблю %s! Попробуйте снова:%n", ship.name);
+                            }
+                        }
+                    }
+
+                } else {
+                    System.out.printf("Вы промахнулись! Попробуйте снова!%n");
+                }
+                if (playerArr[oppositePlayer].allShipsSunk()) {
+                    winGame = true;
+                    endGame(playerArr[currentPlayer].name);
+                }
+                clearScreenAfterEnter(input);
+
+                if (currentPlayer == 0) {
+                    currentPlayer = 1;
+                    oppositePlayer = 0;
+                } else {
+                    currentPlayer = 0;
+                    oppositePlayer = 1;
+                }
+
+            }
+        }
+        System.out.printf("Вы потопили последний вражеский корабль. Вы победили! Поздравляем%n");
+    }
+    public static int convertRowInput(String coordinate) {
+        return 1;
+    }
+
+    public static int convertColumnInput(String coordinate) {
+        return 9;
+    }
+    static void clearScreenAfterEnter(Scanner input) {
+        System.out.println("Нажмите Enter и уступите ход другому игроку");
+        String junk = input.nextLine();
+        junk = input.nextLine();
+        for (int i = 0; i < 100; i++) {
+            System.out.println();
+        }
+    }
+    static void endGame(String name) {
+        System.out.printf("Вы потопили последний вражеский корабль. Вы победили! Поздравляем %s!%n", name);
+        System.exit(0);
     }
 }
--- src/test/java/OthersTest.java
+++ src/test/java/OthersTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static com.company.main.Main.convertColumnInput;
+import static com.company.main.Main.convertRowInput;
 import static org.junit.Assert.*;
 
 public class OthersTest {
@@ -16,28 +18,19 @@ public class OthersTest {
     void testAllShipsSunk() {
         Player player = new Player(1);
 
-        Gameboard gameboard = new Gameboard();
-        char[][] gm = gameboard.getGameBoard();
-        char[][] empty = new char[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                empty[i][j] = '~';
-            }
-        }
+        Battleships battleships1 = new Battleships(Fleet.values()[0].name(), Fleet.values()[0].getLength());
+        battleships1.storeShipCoordinates(3,3);
+        battleships1.storeShipCoordinates(3,4);
+        battleships1.storeShipCoordinates(3,5);
+        battleships1.storeShipCoordinates(3,6);
+        battleships1.storeShipCoordinates(3,7);
 
-        Battleships battleships = new Battleships(Fleet.values()[0].name(), Fleet.values()[0].getLength());
-        battleships.storeShipCoordinates(3,3);
-        battleships.storeShipCoordinates(3,4);
-        battleships.storeShipCoordinates(3,5);
-        battleships.storeShipCoordinates(3,6);
-        battleships.storeShipCoordinates(3,7);
-
-        player.ships.add(battleships);
-        battleships.removeShipCoordinate(3,3);
-        battleships.removeShipCoordinate(3,4);
-        battleships.removeShipCoordinate(3,5);
-        battleships.removeShipCoordinate(3,6);
-        battleships.removeShipCoordinate(3,7);
+        player.ships.add(battleships1);
+        battleships1.removeShipCoordinate(3,3);
+        battleships1.removeShipCoordinate(3,4);
+        battleships1.removeShipCoordinate(3,5);
+        battleships1.removeShipCoordinate(3,6);
+        battleships1.removeShipCoordinate(3,7);
 
         assertTrue(player.allShipsSunk());
     }
@@ -47,27 +40,77 @@ public class OthersTest {
     void testAllShipsSunkFalse() {
         Player player = new Player(1);
 
-        Gameboard gameboard = new Gameboard();
-        char[][] gm = gameboard.getGameBoard();
-        char[][] empty = new char[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                empty[i][j] = '~';
-            }
-        }
+        Battleships battleships1 = new Battleships(Fleet.values()[0].name(), Fleet.values()[0].getLength());
+        battleships1.storeShipCoordinates(3,3);
+        battleships1.storeShipCoordinates(3,4);
+        battleships1.storeShipCoordinates(3,5);
+        battleships1.storeShipCoordinates(3,6);
+        battleships1.storeShipCoordinates(3,7);
 
-        Battleships battleships = new Battleships(Fleet.values()[0].name(), Fleet.values()[0].getLength());
-        battleships.storeShipCoordinates(3,3);
-        battleships.storeShipCoordinates(3,4);
-        battleships.storeShipCoordinates(3,5);
-        battleships.storeShipCoordinates(3,6);
-        battleships.storeShipCoordinates(3,7);
-
-        player.ships.add(battleships);
-        battleships.removeShipCoordinate(3,3);
-        battleships.removeShipCoordinate(3,4);
-        battleships.removeShipCoordinate(3,5);
+        player.ships.add(battleships1);
+        battleships1.removeShipCoordinate(3,3);
+        battleships1.removeShipCoordinate(3,4);
+        battleships1.removeShipCoordinate(3,5);
 
         assertFalse(player.allShipsSunk());
+    }
+
+    @Test
+    @DisplayName("Main: Проверка конвертирования ввода координат по x")
+    void testConvertRowInput() {
+        assertEquals(convertRowInput("J3"), 9);
+        assertEquals(convertRowInput("j3"), 9);
+        assertEquals(convertRowInput("A5"), 0);
+    }
+
+
+
+    @Test
+    @DisplayName("Main: Проверка конвертирования ввода координат по y")
+    void testConvertColumnInput() {
+        assertEquals(convertColumnInput("J3"), 2);
+        assertEquals(convertColumnInput("j3"), 2);
+        assertEquals(convertColumnInput("A5"), 4);
+    }
+
+    @Test
+    @DisplayName("Main: Проверка игры")
+    void testGame() {
+        Player player1 = new Player(1);
+        Player player2 = new Player(2);
+
+        Gameboard gameboard = new Gameboard();
+        char[][] gm1 = player1.gameBoard.getGameBoard();
+        char[][] gm2 = player2.gameBoard.getGameBoard();
+
+        Battleships battleships1 = new Battleships(Fleet.values()[0].name(), Fleet.values()[0].getLength());
+        battleships1.storeShipCoordinates(convertRowInput("C3"), convertColumnInput("C3"));
+        battleships1.storeShipCoordinates(convertRowInput("C3"), convertColumnInput("C4"));
+        battleships1.storeShipCoordinates(convertRowInput("C3"), convertColumnInput("C5"));
+        battleships1.storeShipCoordinates(convertRowInput("C3"), convertColumnInput("C6"));
+        battleships1.storeShipCoordinates(convertRowInput("C3"), convertColumnInput("C7"));
+
+        Battleships battleships2 = new Battleships(Fleet.values()[0].name(), Fleet.values()[0].getLength());
+        battleships2.storeShipCoordinates(convertRowInput("A3"), convertColumnInput("A3"));
+        battleships2.storeShipCoordinates(convertRowInput("A3"), convertColumnInput("A4"));
+        battleships2.storeShipCoordinates(convertRowInput("A3"), convertColumnInput("A5"));
+        battleships2.storeShipCoordinates(convertRowInput("A3"), convertColumnInput("A6"));
+        battleships2.storeShipCoordinates(convertRowInput("A3"), convertColumnInput("A7"));
+
+        player1.ships.add(battleships1);
+        battleships1.removeShipCoordinate(convertRowInput("C3"), convertColumnInput("C3"));
+        battleships1.removeShipCoordinate(convertRowInput("C3"), convertColumnInput("C4"));
+        battleships1.removeShipCoordinate(convertRowInput("C3"), convertColumnInput("C5"));
+        battleships1.removeShipCoordinate(convertRowInput("C3"), convertColumnInput("C6"));
+        battleships1.removeShipCoordinate(convertRowInput("C3"), convertColumnInput("C7"));
+
+        player2.ships.add(battleships2);
+        battleships2.removeShipCoordinate(convertRowInput("A3"), convertColumnInput("A3"));
+        battleships2.removeShipCoordinate(convertRowInput("A3"), convertColumnInput("A4"));
+        battleships2.removeShipCoordinate(convertRowInput("A3"), convertColumnInput("A5"));
+        battleships2.removeShipCoordinate(convertRowInput("A3"), convertColumnInput("A6"));
+
+        assertTrue(player1.allShipsSunk());
+        assertFalse(player2.allShipsSunk());
     }
 }
